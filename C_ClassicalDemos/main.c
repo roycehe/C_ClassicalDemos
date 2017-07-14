90e5efcef98e336c7c8c1d7eba62b32b6b542eb3
roycehe <hexiaowen@macbook-pro.local>
2017-07-14 10:04:26 +0800

    Initial Commit

---
+++ C_ClassicalDemos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_ClassicalDemos
+//
+//  Created by 何晓文 on 2017/7/14.
+//  Copyright © 2017年 何晓文. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
